7f90dc74d567564396a937fa75993ecebeee756d
Ruben Quadros <ruben@Rubens-MacBook-Pro.local>
2021-08-10 18:37:01 +0530

    initial commit

---
+++ app/src/main/java/com/ruben/composition/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ruben.composition
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ruben.composition.ui.theme.CompositionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CompositionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CompositionTheme {
+        Greeting("Android")
+    }
+}
