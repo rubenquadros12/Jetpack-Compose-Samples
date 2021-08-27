4b722603a30fa7b77513629cfc28955b08ab7e47
Ajay K Meher <82014777+ameher-sharechat@users.noreply.github.com>
2021-08-28 01:24:36 +0530

    Live Now Bottom Sheet with Swipe option.

--- app/src/main/java/com/ruben/composition/screens/bottomsheet/LiveNowBottomSheet.kt
+++ app/src/main/java/com/ruben/composition/screens/bottomsheet/LiveNowBottomSheet.kt
@@ -1,7 +1,9 @@
 package com.ruben.composition.screens.bottomsheet
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -9,18 +11,24 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.ruben.composition.R
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Preview
 @ExperimentalMaterialApi
@@ -33,21 +41,7 @@ fun LiveNowBottomSheet() {
 
         BottomSheetScaffold(
                 sheetContent = {
-                    Column(modifier = Modifier
-                            .fillMaxWidth()
-                            .height(400.dp)) {
-                        Box(modifier = Modifier
-                                .height(56.dp)
-                                .fillMaxWidth(), contentAlignment = Alignment.Center) {
-                            Text("Live Now", color = Color.White, fontSize = 20.sp)
-                        }
-
-                        LazyColumn(verticalArrangement = Arrangement.spacedBy(1.dp)) {
-                            items(100) {
-                                LiveUserEntry()
-                            }
-                        }
-                    }
+                    LiveNowContent()
                 },
                 scaffoldState = scaffoldState,
                 sheetPeekHeight = 0.dp,
@@ -81,12 +75,97 @@ fun LiveNowBottomSheet() {
 
 }
 
+@ExperimentalMaterialApi
+@Composable
+private fun LiveNowContent() {
+    Column(modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)) {
+        Box(modifier = Modifier
+                .height(56.dp)
+                .fillMaxWidth(), contentAlignment = Alignment.Center) {
+            Text("Live Now", color = Color.White, fontSize = 20.sp)
+        }
+
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(1.dp)) {
+            items(100) {
+                SwipeableLiveUserEntry()
+            }
+        }
+    }
+}
+
+
+@ExperimentalMaterialApi
 @Preview
 @Composable
-private fun LiveUserEntry() {
+fun SwipeableSample() {
+    val width = 96.dp
+    val squareSize = 48.dp
+
+    val swipeableState = rememberSwipeableState(0)
+    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+    val anchors = mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
+
+    Box(
+            modifier = Modifier
+                    .width(width)
+                    .swipeable(
+                            state = swipeableState,
+                            anchors = anchors,
+                            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                            orientation = Orientation.Horizontal
+                    )
+                    .background(Color.LightGray)
+    ) {
+        Box(
+                Modifier
+                        .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+                        .size(squareSize)
+                        .background(Color.DarkGray)
+        )
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun SwipeableLiveUserEntry() {
+    val squareSize = 72.dp
+
+    val swipeableState = rememberSwipeableState(0)
+    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+    val anchors = mapOf(0f to 0, -sizePx to 1) // Maps anchor points (in px) to states
+
+    Box(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .swipeable(
+                            state = swipeableState,
+                            anchors = anchors,
+                            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                            orientation = Orientation.Horizontal
+                    )
+    ) {
+        Box(modifier = Modifier.fillMaxWidth().background(Color(0xFF131319)), contentAlignment = Alignment.CenterEnd) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.width(72.dp).height(72.dp).clickable {  }) {
+                Icon(Icons.Default.Delete, contentDescription = "Block the user", tint = Color(0xFFFC3361))
+                Text(text = "Block", color = Color(0xFFFC3361))
+            }
+        }
+        LiveUserEntry(swipeableState)
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+private fun LiveUserEntry(swipeableState: SwipeableState<Int>) {
     Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+            .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
             .fillMaxWidth()
+            .background(Color(0xFF000000))
             .wrapContentHeight()
+            .zIndex(1.0f)
             .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
             .clickable { }) {
         Surface(
